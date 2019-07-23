1e751b09c53f320f1dbba03c6af0fa57f5d8a412
bergi <bergi@axolotlfarm.org>
2019-07-23 10:54:39 +0200

    added comments, added entry in readme

--- examples/ngc2392.js
+++ examples/ngc2392.js
@@ -9,10 +9,12 @@ const ns = {
   xsd: namespace('http://www.w3.org/2001/XMLSchema#')
 }
 
+// Literal loader that simple converts the value of the Literal using parseInt
 function integerLoader (term) {
   return parseInt(term.value)
 }
 
+// Node loader that creates a nebular object (label, distance) using nested loaders
 async function nebulaLoader (term, dataset, { loaderRegistry }){
   const node = cf(dataset, term)
 
@@ -23,17 +25,20 @@ async function nebulaLoader (term, dataset, { loaderRegistry }){
 }
 
 async function main () {
+  // create the registry...
   const registry = new LoaderRegistry()
 
+  // ...and register the loaders
   registry.registerLiteralLoader(ns.xsd.integer, integerLoader)
   registry.registerNodeLoader(ns.example.Nebula, nebulaLoader)
 
+  // create the quads for the nebular object
   const ngc2392 = cf(rdf.dataset(), ns.example('ngc/2392'))
   ngc2392.addOut(ns.rdf.type, ns.example.Nebula)
   ngc2392.addOut(ns.example.label, rdf.literal('Clownface Nebula'))
   ngc2392.addOut(ns.example.distance, rdf.literal('2870', ns.xsd.integer))
 
-  // should return an object with a label and distance property
+  // use the loader to convert the quads to a nebular object with a label and distance property
   const result = await registry.load(ngc2392)
 
   console.log(result)
